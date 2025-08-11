e3a45394bb712d6d5c8007d3343ba43bcf0f6b79
Santiago <santiusaj11@gmail.com>
2025-08-11 14:10:44 -0300

    authentication with express session and cookies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,12 +4,30 @@ import notesRoutes from "./routes/notes.js";
 import userRoutes from "./routes/users.js";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
+import env from "./util/validateEnv.js";
+import MongoStore from "connect-mongo";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGO_CONNECTION_STRING,
+    }),
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", notesRoutes);
